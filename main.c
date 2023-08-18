50fb8d761cb3b11ae81a6abb438dbc645f25a690
Younes Bouladhane <younes.bouladhane@coolbet.com>
2023-08-18 18:51:44 +0300

    add comment another function and call it

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int toz(int taz) {
+return taz * taz;
+}
+
+/*
+this is the inside comment you fuckers want :D
+*/
+
 int main() {
-char *code = "#include <stdio.h>%c%cint main() {%cchar *code = %c%s%c;%cprintf(code, 10 ,10, 10, 34, code, 34, 10, 10, 10);%c}%c";
-printf(code, 10 ,10, 10, 34, code, 34, 10, 10, 10);
+/*
+and this is the outside one.
+*/
+char *code = "#include <stdio.h>%c%cint toz(int taz) {%creturn taz * taz;%c}%c%c/*%cthis is the inside comment you fuckers want :D%c*/%c%cint main() {%c/*%cand this is the outside one.%c*/%cchar *code = %c%s%c;%cprintf(code, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10);%ctoz(1);%c}%c";
+printf(code, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10);
+toz(1);
 }
