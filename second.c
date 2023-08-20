41a4622f99850bb3733ca3dad5716e216b3691e3
Younes Bouladhane <younes.bouladhane@coolbet.com>
2023-08-20 20:19:37 +0300

    grace working

---
+++ second.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define decode(t, n, a, k, e, t1) a##e##k##n
+#define liJaBismillah decode(z, n, m, i, a, t)
+#define CLONE(x) int liJaBismillah() { void* ptr = fopen("Grace_kid.c", "w"); fputs(x, ptr); fputs("\n\n/*\n     El Caid Motorhead \\,,/(>.<)\\,,/\n*/\n\nCLONE("#x");", ptr);}
+
+/*
+     El Caid Motorhead \,,/(>.<)\,,/
+*/
+
+CLONE("#include <stdio.h>\n#define decode(t, n, a, k, e, t1) a##e##k##n\n#define liJaBismillah decode(z, n, m, i, a, t)\n#define CLONE(x) int liJaBismillah() { void* ptr = fopen(\"Grace_kid.c\", \"w\"); fputs(x, ptr); fputs(\"\\n\\n/*\\n     El Caid Motorhead \\\\,,/(>.<)\\\\,,/\\n*/\\n\\nCLONE(\"#x\");\", ptr);}");
